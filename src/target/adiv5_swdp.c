f85fc0e4e349ddea4edc032c92e11b9ed603b981
JojoS62 <sn2010@onlinehome.de>
2020-10-01 23:44:04 +0200

    back to original ack wait handling

--- src/target/adiv5_swdp.c
+++ src/target/adiv5_swdp.c
@@ -148,25 +148,24 @@ uint32_t firmware_swdp_low_access(ADIv5_DP_t *dp, uint8_t RnW,
 		swd_proc.swdptap_seq_out(request, 8);
 		ack = swd_proc.swdptap_seq_in(3);
 		
-		if ((ack == SWDP_ACK_WAIT) || (ack == SWDP_ACK_FAULT)) {
-			// finish pending operation first
-			if (RnW) {
-				uint32_t dummy_response = 0;
-				swd_proc.swdptap_seq_in_parity(&dummy_response, 32);	// send 31+1+turnaround
-			} else {
-				swd_proc.swdptap_seq_out_parity(0, 32);		// send turnaround+32+1 bit
-				swd_proc.swdptap_seq_out(0, 2);				// send 2 trailing bit
-			}
+		// if ((ack == SWDP_ACK_WAIT) || (ack == SWDP_ACK_FAULT)) {
+		// 	// finish pending operation first
+		// 	if (RnW) {
+		// 		uint32_t dummy_response = 0;
+		// 		swd_proc.swdptap_seq_in_parity(&dummy_response, 32);	// send 31+1+turnaround
+		// 	} else {
+		// 		swd_proc.swdptap_seq_out_parity(0, 32);		// send turnaround+32+1 bit
+		// 		swd_proc.swdptap_seq_out(0, 2);				// send 2 trailing bit
+		// 	}
+		// }	
 
-
-			/* On fault, abort() and repeat the command once.*/
-			if (ack == SWDP_ACK_FAULT) {
-				printf("*** ACK Fault, retry\n");
-				firmware_swdp_error(dp);
-				swd_proc.swdptap_seq_out(request, 8);
-				ack = swd_proc.swdptap_seq_in(3);
-			}
-		}	
+		/* On fault, abort() and repeat the command once.*/
+		if (ack == SWDP_ACK_FAULT) {
+			printf("*** ACK Fault, retry\n");
+			firmware_swdp_error(dp);
+			swd_proc.swdptap_seq_out(request, 8);
+			ack = swd_proc.swdptap_seq_in(3);
+		}
 	} while ((ack == SWDP_ACK_WAIT)  && !platform_timeout_is_expired(&timeout));
 
 	if (ack == SWDP_ACK_WAIT) {
