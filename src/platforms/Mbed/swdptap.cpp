1136ad41713d26da3892791ae50f029a7937e806
JojoS62 <sn2010@onlinehome.de>
2020-10-01 22:11:16 +0200

    add clockDelay and 2MHz setting

--- src/platforms/Mbed/swdptap.cpp
+++ src/platforms/Mbed/swdptap.cpp
@@ -31,15 +31,66 @@ enum {
 };
 
 extern "C" {
-static void swdptap_turnaround(int dir) __attribute__ ((optimize(1)));
-static uint32_t swdptap_seq_in(int ticks) __attribute__ ((optimize(1)));
-static bool swdptap_seq_in_parity(uint32_t *ret, int ticks)	__attribute__ ((optimize(1)));
-static void swdptap_seq_out(uint32_t MS, int ticks)	__attribute__ ((optimize(1)));
-static void swdptap_seq_out_parity(uint32_t MS, int ticks)	__attribute__ ((optimize(1)));
+// static void swdptap_turnaround(int dir) __attribute__ ((optimize(1)));
+// static uint32_t swdptap_seq_in(int ticks) __attribute__ ((optimize(1)));
+// static bool swdptap_seq_in_parity(uint32_t *ret, int ticks)	__attribute__ ((optimize(1)));
+// static void swdptap_seq_out(uint32_t MS, int ticks)	__attribute__ ((optimize(1)));
+// static void swdptap_seq_out_parity(uint32_t MS, int ticks)	__attribute__ ((optimize(1)));
+
+static void swdptap_turnaround(int dir);
+static uint32_t swdptap_seq_in(int ticks);
+static bool swdptap_seq_in_parity(uint32_t *ret, int ticks);
+static void swdptap_seq_out(uint32_t MS, int ticks);
+static void swdptap_seq_out_parity(uint32_t MS, int ticks);
 }
 
-DigitalOut 	swdClk(PB_7, 0);
-DigitalInOut swdDIO(PB_8, PIN_OUTPUT, PullNone, 1);
+#define GPIO_OSPEED_2MHZ		0x0
+#define GPIO_OSPEED_25MHZ		0x1
+#define GPIO_OSPEED_50MHZ		0x2
+#define GPIO_OSPEED_100MHZ		0x3
+
+class FastDigitalOut : public DigitalOut {
+public:	
+	FastDigitalOut(PinName pin, int value) :
+		DigitalOut(pin, value) {
+			// init with low speed
+			pin_function(pin, STM_PIN_DEFINE_SPEED_EXT(STM_MODE_OUTPUT_PP, GPIO_NOPULL, 0, 0, 0, GPIO_OSPEED_2MHZ));
+	}
+
+	DigitalOut &operator= (int value)
+    {
+        // Underlying write is thread safe
+        write(value);
+        return *this;
+    }
+};
+
+class FastDigitalInOut : public DigitalInOut {
+public:	
+	FastDigitalInOut(PinName pin, PinDirection direction, PinMode mode, int value) :
+		DigitalInOut(pin, direction, mode, value) {
+			// init with low speed
+			pin_function(pin, STM_PIN_DEFINE_SPEED_EXT(STM_MODE_OUTPUT_PP, GPIO_NOPULL, 0, 0, 0, GPIO_OSPEED_2MHZ));
+	}
+
+	DigitalInOut &operator= (int value)
+    {
+        // Underlying write is thread safe
+        write(value);
+        return *this;
+    }
+};
+
+FastDigitalOut 	swdClk(PB_7, 0);
+FastDigitalInOut swdDIO(PB_8, PIN_OUTPUT, PullNone, 1);
+
+constexpr uint16_t DELAY_CYCLES = 2;
+void clockDelay(uint16_t n) 
+{
+	while(--n) {
+		__NOP();
+	};
+}
 
 static void swdptap_turnaround(int dir)
 {
@@ -54,12 +105,13 @@ static void swdptap_turnaround(int dir)
 	}
 	
 	swdClk = 1;
-	swdClk = 1;
-	swdClk = 1;
+	clockDelay(DELAY_CYCLES);
 	swdClk = 0;
+	clockDelay(DELAY_CYCLES);
 
 	if (dir == SWDIO_STATUS_DRIVE) {
 		swdDIO.output();
+		swdDIO=1;
 	}
 }
 
@@ -76,7 +128,9 @@ static uint32_t swdptap_seq_in(int ticks)
 			ret |= index;
 		index <<= 1;
 		swdClk = 1;
+		clockDelay(DELAY_CYCLES);
 		swdClk = 0;
+		clockDelay(DELAY_CYCLES);
 	}
 
 	return ret;
@@ -85,14 +139,16 @@ static uint32_t swdptap_seq_in(int ticks)
 static bool swdptap_seq_in_parity(uint32_t *ret, int ticks)
 {
 	*ret = swdptap_seq_in(ticks);
+	int parity_bit = swdDIO & 1;
+	swdClk = 1;
+	clockDelay(DELAY_CYCLES);
+	swdClk = 0;
+	clockDelay(DELAY_CYCLES);
 
 	int parity = __builtin_popcount(*ret);
-	int parity_bit = swdDIO & 1;
 	if (parity_bit) {
 		parity++;
 	}
-	swdClk = 1;
-	swdClk = 0;
 
 	if (ticks == 32) {
 		swdptap_turnaround(SWDIO_STATUS_DRIVE);
@@ -104,12 +160,19 @@ static bool swdptap_seq_in_parity(uint32_t *ret, int ticks)
 static void swdptap_seq_out(uint32_t MS, int ticks)
 {
 	swdptap_turnaround(SWDIO_STATUS_DRIVE);
+	bool trailing_bits = ticks==2;
 
 	while (ticks--) {
 		swdDIO = MS & 1;
 		MS >>= 1;
 		swdClk = 1;
+		clockDelay(DELAY_CYCLES);
 		swdClk = 0;
+		clockDelay(DELAY_CYCLES);
+	}
+
+	if (trailing_bits) {
+		swdDIO = 1;
 	}
 }
 
@@ -120,7 +183,9 @@ static void swdptap_seq_out_parity(uint32_t MS, int ticks)
 	swdptap_seq_out(MS, ticks);
 	swdDIO = parity & 1;
 	swdClk = 1;
+	clockDelay(DELAY_CYCLES);
 	swdClk = 0;
+	clockDelay(DELAY_CYCLES);
 }
 
 swd_proc_t swd_proc;
