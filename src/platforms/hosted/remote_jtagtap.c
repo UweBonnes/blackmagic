12d898e6b6c63182e27890c3589336da21d24c92
Uwe Bonnes <bon@elektron.ikp.physik.tu-darmstadt.de>
2021-07-17 20:59:48 +0200

    jtagtap_tdi_tdo_seq/remote: Allow more than 32 ticks in one call.

--- src/platforms/hosted/remote_jtagtap.c
+++ src/platforms/hosted/remote_jtagtap.c
@@ -104,35 +104,52 @@ static void jtagtap_tms_seq(uint32_t MS, int ticks)
     }
 }
 
+/* At least up to v1.7.1-233, remote handles only up to 32 ticks in one
+ * call. Break up large calls.
+ *
+ * FIXME: Provide and  test for faster call and keep fallback
+ */
 static void jtagtap_tdi_tdo_seq(
 	uint8_t *DO, const uint8_t final_tms, const uint8_t *DI, int ticks)
 {
 	uint8_t construct[REMOTE_MAX_MSG_SIZE];
 	int s;
 
-	uint64_t DIl=*(uint64_t *)DI;
+	if(!ticks || (!DI && !DO))
+		return;
+	uint32_t *DIW = (uint32_t *)DI;
+	uint32_t *DOW = (uint32_t *)DO;
+	while (ticks) {
+		int portion;
+		if (ticks < 33)
+			portion = ticks;
+		else
+			portion = 32;
+		ticks -= portion;
+		uint64_t DIl;
+		if (DI)
+			DIl = *DIW++;
+		else
+			DIl = 0;
+		/* Reduce the length of DI according to the bits we're transmitting */
+		DIl &= ((1LL << (portion + 1)) - 1);
+		s = snprintf((char *)construct, REMOTE_MAX_MSG_SIZE,
+					 REMOTE_JTAG_TDIDO_STR,
+					 (!ticks && final_tms) ?
+					 REMOTE_TDITDO_TMS : REMOTE_TDITDO_NOTMS,
+					 portion, DIl);
+		platform_buffer_write(construct,s);
 
-	if(!ticks || !DI) return;
-
-	/* Reduce the length of DI according to the bits we're transmitting */
-	DIl &= (1LL << (ticks + 1))-1;
-
-	s = snprintf((char *)construct, REMOTE_MAX_MSG_SIZE,
-				 REMOTE_JTAG_TDIDO_STR,
-				 final_tms ? REMOTE_TDITDO_TMS : REMOTE_TDITDO_NOTMS,
-				 ticks, DIl);
-	platform_buffer_write(construct,s);
-
-	s = platform_buffer_read(construct, REMOTE_MAX_MSG_SIZE);
-	if ((!s) || (construct[0] == REMOTE_RESP_ERR)) {
-		DEBUG_WARN("jtagtap_tms_seq failed, error %s\n",
-				s ? (char *)&(construct[1]) : "unknown");
-		exit(-1);
-    }
-
-	if (DO) {
-		uint64_t DOl = remotehston(-1, (char *)&construct[1]);
-		*(uint64_t *)DO = DOl;
+		s = platform_buffer_read(construct, REMOTE_MAX_MSG_SIZE);
+		if ((!s) || (construct[0] == REMOTE_RESP_ERR)) {
+			DEBUG_WARN("jtagtap_tms_seq failed, error %s\n",
+					   s ? (char *)&(construct[1]) : "unknown");
+			exit(-1);
+		}
+		if (DO) {
+			uint64_t DOl = remotehston(-1, (char *)&construct[1]);
+			*DOW++ = DOl;
+		}
 	}
 }
 
