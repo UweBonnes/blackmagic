2ed744e3721969f36523538feed1345ec95a5c6b
Uwe Bonnes <bon@elektron.ikp.physik.tu-darmstadt.de>
2021-10-25 12:22:35 +0200

    libftdi_swdptap: Rework. use fast read.
    No AdiV5 overrun detection is used. When reading the block of memory, only
    the MPSSE cmmands are written to the device and readback and evaluation is
    done for the whole block.

--- src/platforms/hosted/libftdi_swdptap.c
+++ src/platforms/hosted/libftdi_swdptap.c
@@ -47,7 +47,8 @@ static void swdptap_turnaround(enum swdio_status dir)
 	if (dir == olddir)
 		return;
 	olddir = dir;
-	DEBUG_PROBE("Turnaround %s\n", (dir == SWDIO_STATUS_FLOAT) ? "float": "drive");
+	uint8_t cmd[16], *p = cmd;
+	DEBUG_PROBE("Turnaround %s: ", (dir == SWDIO_STATUS_FLOAT) ? "float": "drive");
 	if (do_mpsse) {
 		if (dir == SWDIO_STATUS_FLOAT)	/* SWDIO goes to input */ {
 			active_state.data_low |=  active_cable->mpsse_swd_read.set_data_low | MPSSE_DO;
@@ -55,30 +56,30 @@ static void swdptap_turnaround(enum swdio_status dir)
 			active_state.ddr_low &= ~MPSSE_DO;
 			active_state.data_high |=  active_cable->mpsse_swd_read.set_data_high;
 			active_state.data_high &= ~active_cable->mpsse_swd_read.clr_data_high;
-			uint8_t cmd_read[6] = {
-				SET_BITS_LOW,  active_state.data_low,
-				active_state.ddr_low,
-				SET_BITS_HIGH, active_state.data_high, active_state.ddr_high};
-			libftdi_buffer_write(cmd_read, 6);
+			*p++ = SET_BITS_LOW;
+			*p++ = active_state.data_low;
+			*p++ = active_state.ddr_low;
+			*p++ = SET_BITS_HIGH;
+			*p++ = active_state.data_high;
+			*p++ = active_state.ddr_high;
 		}
-		uint8_t cmd[] = {MPSSE_TDO_SHIFT, 0, 0}; /* One clock cycle */
-		libftdi_buffer_write(cmd, sizeof(cmd));
+		*p++ = MPSSE_TDO_SHIFT;
+		*p++ = 0;
+		*p++ = 0; /* One clock cycle */
 		if (dir == SWDIO_STATUS_DRIVE)  /* SWDIO goes to output */ {
 			active_state.data_low |=  active_cable->mpsse_swd_write.set_data_low | MPSSE_DO;
 			active_state.data_low &= ~active_cable->mpsse_swd_write.clr_data_low;
 			active_state.ddr_low |= MPSSE_DO;
 			active_state.data_high |=  active_cable->mpsse_swd_write.set_data_high;
 			active_state.data_high &= ~active_cable->mpsse_swd_write.clr_data_high;
-			uint8_t cmd_write[6] = {
-				SET_BITS_LOW,  active_state.data_low,
-				active_state.ddr_low,
-				SET_BITS_HIGH, active_state.data_high, active_state.ddr_high};
-			libftdi_buffer_write(cmd_write, 6);
+			*p++ = SET_BITS_LOW;
+			*p++ = active_state.data_low;
+			*p++ = active_state.ddr_low;
+			*p++ = SET_BITS_HIGH;
+			*p++ = active_state.data_high;
+			*p++ = active_state.ddr_high;
 		}
 	} else {
-		uint8_t cmd[9];
-		int index = 0;
-
 		if(dir == SWDIO_STATUS_FLOAT)	  { /* SWDIO goes to input */
 			if (direct_bb_swd) {
 				active_state.data_low |=  MPSSE_CS;
@@ -89,17 +90,17 @@ static void swdptap_turnaround(enum swdio_status dir)
 				active_state.data_high |=  active_cable->bb_swd_read.set_data_high;
 				active_state.data_high &= ~active_cable->bb_swd_read.clr_data_high;
 			}
-			cmd[index++] = SET_BITS_LOW;
-			cmd[index++] = active_state.data_low;
-			cmd[index++] = active_state.ddr_low;
-			cmd[index++] = SET_BITS_HIGH;
-			cmd[index++] = active_state.data_high;
-			cmd[index++] = active_state.ddr_high;
+			*p++ = SET_BITS_LOW;
+			*p++ = active_state.data_low;
+			*p++ = active_state.ddr_low;
+			*p++ = SET_BITS_HIGH;
+			*p++ = active_state.data_high;
+			*p++ = active_state.ddr_high;
 		}
 		/* One clock cycle */
-		cmd[index++] = MPSSE_TMS_SHIFT;
-		cmd[index++] = 0;
-		cmd[index++] = 0;
+		*p++ = MPSSE_TMS_SHIFT;
+		*p++ = 0;
+		*p++ = 0;
 		if (dir == SWDIO_STATUS_DRIVE) {
 			if (direct_bb_swd) {
 				active_state.data_low |=  MPSSE_CS;
@@ -110,15 +111,15 @@ static void swdptap_turnaround(enum swdio_status dir)
 				active_state.data_high |=  active_cable->bb_swd_write.set_data_high;
 				active_state.data_high &= ~active_cable->bb_swd_write.clr_data_high;
 			}
-			cmd[index++] = SET_BITS_LOW;
-			cmd[index++] = active_state.data_low;
-			cmd[index++] = active_state.ddr_low;
-			cmd[index++] = SET_BITS_HIGH;
-			cmd[index++] = active_state.data_high;
-			cmd[index++] = active_state.ddr_high;
+			*p++ = SET_BITS_LOW;
+			*p++ = active_state.data_low;
+			*p++ = active_state.ddr_low;
+			*p++ = SET_BITS_HIGH;
+			*p++ = active_state.data_high;
+			*p++ = active_state.ddr_high;
 		}
-		libftdi_buffer_write(cmd, index);
 	}
+	libftdi_buffer_write(cmd, p - cmd);
 }
 
 static bool swdptap_seq_in_parity(uint32_t *res, int ticks);
@@ -163,6 +164,170 @@ bool libftdi_swd_possible(bool *do_mpsse, bool *direct_bb_swd)
 			*direct_bb_swd = true;
 	}
 	return true;
+}
+
+/* Construct stream of MPSSE commands. Read back later
+ * MPSSE: 29 Bytes per word, else 156 Byte
+ */
+static void mpsse_fast_swdp_low_read(uint16_t addr)
+{
+	swdptap_turnaround(SWDIO_STATUS_DRIVE); /* 9 */
+	unsigned int request = make_packet_request(ADIV5_LOW_READ, addr);
+	if (do_mpsse) {
+		uint8_t cmd[16] = {
+			MPSSE_DO_WRITE | MPSSE_WRITE_NEG | MPSSE_LSB,
+			1 - 1,
+			0,
+			request
+			};
+		libftdi_buffer_write(cmd, 4); /* 4 Byte */
+		swdptap_turnaround(SWDIO_STATUS_FLOAT); /* 9 Byte */
+		unsigned int index = 0;
+		cmd[index++] = MPSSE_DO_READ | MPSSE_LSB | MPSSE_BITMODE;
+		cmd[index++] = 3 - 1;
+		cmd[index++] = MPSSE_DO_READ | MPSSE_LSB;
+		cmd[index++] = 4 - 1;
+		cmd[index++] = 0;
+		cmd[index++] = MPSSE_DO_READ | MPSSE_LSB | MPSSE_BITMODE;
+		cmd[index++] = 1 - 1;
+		libftdi_buffer_write(cmd, index); /* 7 Byte */
+	} else {
+		uint8_t cmd[16];
+		unsigned int index = 0;
+		cmd[index++] = MPSSE_TMS_SHIFT;
+		cmd[index++] = 7 - 1;
+		cmd[index++] = request & 0x7f;
+		request >>= 7;
+		cmd[index++] = MPSSE_TMS_SHIFT;
+		cmd[index++] = 1 - 1;
+		cmd[index++] = request & 0x7f;
+		libftdi_buffer_write(cmd, index); /* 6 Byte */
+		swdptap_turnaround(SWDIO_STATUS_FLOAT); /* 9 Byte*/
+		cmd[0] = active_cable->bb_swdio_in_port_cmd;
+		cmd[1] = MPSSE_TMS_SHIFT;
+		cmd[2] = 0;
+		cmd[3] = 0;
+		int  bits = 36;
+		while (bits--) {
+			libftdi_buffer_write(cmd, sizeof(cmd)); /* 138 Bytes*/
+		}
+	}
+}
+
+static int eval(void *dest, int words)
+{
+	uint8_t buffer[4096], *p = buffer;
+	uint32_t *data = (uint32_t*)dest;
+	int ret = 0;
+	if (do_mpsse) {
+		int size = libftdi_buffer_read(buffer, words * 6);
+		int items = size / 6; /* 1 Byte ack, 4 byte data, 1 byte parity */
+		ret = items;
+		if (size - ret * 6) {
+			DEBUG_WARN("Invalid read size %d\n", size);
+			return 0;
+		}
+		while (items--) {
+			uint8_t ack = (*p++ >> 5);
+			if (ack != 1) {
+				DEBUG_WARN("Invalid ack %x\n", ack);
+				return 0;
+			}
+			uint32_t word = *p++;
+			word |= *p++ <<  8;
+			word |= *p++ << 16;
+			word |= *p++ << 24;
+			int parity = (*p++ & 0x80) ? 1 : 0; /* parity */
+			if ((__builtin_popcount(word) + parity) & 1) {
+				DEBUG_WARN("Invalid parity %08" PRIx32": %x\n", word, parity);
+				return 0;
+			}
+			*data++ = word;
+		}
+	} else {
+		int size = libftdi_buffer_read(buffer, words * 36);
+		int items = size / 36; /* 3 Byte ack, 32 byte data, 1 byte parity */
+		ret = items;
+		if (size - ret * 36) {
+			DEBUG_WARN("Invalid read size %d\n", size);
+			return 0;
+		}
+		while (items--) {
+			uint8_t ack = (*p++ & 0x80);
+			ack |= (*p++ & 0x80) << 1;
+			ack |= (*p++ & 0x80) << 2;
+			if (ack != 1) {
+				DEBUG_WARN("Invalid ack %x\n", ack);
+				return 0;
+			}
+			uint32_t word = 0;
+			for (int i = 0; i < 32; i++)
+				word += (*p++ & 0x80) ? (1 << i) : 0;
+			*data++ = word;
+			int parity = *p++ & 0x80;
+			if ((__builtin_popcount(word) + parity) & 1) {
+				DEBUG_WARN("Invalid parity %08" PRIx32": %x\n", word, parity);
+				return 0;
+			}
+		}
+	}
+	return ret;
+}
+
+/* Do not evaluate the device ack on every word. */
+static void mpsse_mem_read(ADIv5_AP_t *ap, void *dest, uint32_t src, size_t len)
+{
+	if ((src & 3) || (len & 3))
+		return firmware_mem_read(ap, dest, src, len);
+	len >>= ALIGN_WORD;
+	uint32_t csw = ap->csw | ADIV5_AP_CSW_ADDRINC_SINGLE | ADIV5_AP_CSW_SIZE_WORD;
+	ap->dp->ap_write(ap, ADIV5_AP_CSW, csw);
+	ap->dp->low_access(ap->dp, ADIV5_LOW_WRITE, ADIV5_AP_TAR, src);
+	ap->dp->low_access(ap->dp, ADIV5_LOW_READ, ADIV5_AP_DRW, 0);
+	/* Check if adress causes fault*/
+	ap->dp->low_access(ap->dp, ADIV5_LOW_READ, ADIV5_DP_RDBUFF, 0);
+	if (ap->dp->fault)
+		return;
+	int words = 0;
+	while (--len) {
+		mpsse_fast_swdp_low_read(ADIV5_AP_DRW);
+		src += (1 << ALIGN_WORD);
+		words++;
+		if (!do_mpsse && (words >= (4095 / 36))) {
+				int items = eval(dest, words);
+				if (!items) {
+					ap->dp->fault = 1;
+					return;
+				}
+				dest += items  * 4;
+				words = 0;
+		}
+		/* Check for 10 bit address overflow */
+		if (!(src & 0x3ff)) {
+			if (words) {
+				int items = eval(dest, words);
+				if (!items) {
+					ap->dp->fault = 1;
+					return;
+				}
+				dest += items  * 4;
+				words = 0;
+			}
+			adiv5_dp_low_access(ap->dp, ADIV5_LOW_WRITE, ADIV5_AP_TAR, src);
+			adiv5_dp_low_access(ap->dp, ADIV5_LOW_READ, ADIV5_AP_DRW, 0);
+			/* Check if adress causes fault*/
+			ap->dp->low_access(ap->dp, ADIV5_LOW_READ, ADIV5_DP_RDBUFF, 0);
+			if (ap->dp->fault)
+				return;
+		}
+	}
+	mpsse_fast_swdp_low_read(ADIV5_DP_RDBUFF);
+	words++;
+	int items = eval(dest, words);
+	if (!items) {
+		ap->dp->fault = 1;
+		return;
+	}
 }
 
 int libftdi_swdptap_init(ADIv5_DP_t *dp)
@@ -213,6 +378,7 @@ int libftdi_swdptap_init(ADIv5_DP_t *dp)
 	dp->error = firmware_swdp_error;
 	dp->low_access = firmware_swdp_low_access;
 	dp->abort = firmware_swdp_abort;
+	dp->mem_read = mpsse_mem_read;
 	return 0;
 }
 
